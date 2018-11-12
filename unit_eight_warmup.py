df37530702f3b294cedbe22064b03bb28623a146
Flint <flint@iMac1.local>
2018-11-12 08:26:34 -0500

    added convert function

--- unit_eight_warmup.py
+++ unit_eight_warmup.py
@@ -7,6 +7,13 @@ fer = tkinter.StringVar()
 
 cel = tkinter.StringVar()
 
+
+def f_to_c():
+    fer_temp = int(fer.get())
+    cel_temp = (5/9) * (fer_temp - 32)
+    cel.set(str(cel_temp))
+
+
 degree_f = tkinter.Label(root, text="degrees F:")
 degree_f.grid(row=1, column=1)
 
@@ -16,6 +23,10 @@ f_entry.grid(row=1, column=2)
 degree_c = tkinter.Label(root, text="degrees C:")
 degree_c.grid(row=2, column=1)
 
+c_answer = tkinter.Label(root, textvariable=cel)
+c_answer.grid(row=2, column=2)
 
+convert_button = tkinter.Button(root, text="convert", command=f_to_c)
+convert_button.grid(row=3, column=1)
 
 root.mainloop()
